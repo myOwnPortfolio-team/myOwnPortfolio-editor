3492fd12c0b6674fb88f96d0f978c37aaec7d1ca
Nistof <nistof.contact@gmail.com>
2017-10-12 21:27:47 +0200

    UPDATE - Cleaning HTML for distribution

--- tasks/package.js
+++ tasks/package.js
@@ -1,4 +1,5 @@
 const gulp = require('gulp');
+const deleteLines = require('gulp-delete-lines');
 
 const appPath = './app';
 const distPath = './dist';
@@ -13,6 +14,11 @@ gulp.task('package:main', () => gulp
 
 gulp.task('package:html', () => gulp
   .src(`${appPath}/**/*.html`)
+  .pipe(deleteLines({
+    filters: [
+      /<script\s+src=["']http:\/\/localhost:8080\/dist\/package\/app\/js\/bundle\.js/i,
+    ],
+  }))
   .pipe(gulp.dest(`${distPath}/package/app/`)));
 
 gulp.task('package', ['package:json', 'package:main', 'package:html']);
