a390d7d7b11474dcab3e2ba5964d74fb0afb2fe9
Nistof <nistof.contact@gmail.com>
2017-10-19 18:21:05 +0200

    UPDATE - Reading content from a JSON in the core repo

--- app/classes/App.jsx
+++ app/classes/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import { Header } from 'semantic-ui-react';
+import axios from 'axios';
 
 import SideBar from './Sidebar.jsx';
 
 class App extends React.Component {
+  componentDidMount() {
+    axios.get('https://api.github.com/repos/myOwnPortfolio-team/myOwnPortfolio-core/contents/app/modules/about/json_schema/content.json', {
+      headers: { Accept: 'application/vnd.github.VERSION.raw' },
+    }).then((res) => {
+      console.log(res.data);
+    });
+  }
+
   render() {
     return (
       <div className="app">
