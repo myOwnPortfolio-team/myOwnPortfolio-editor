48a82967047df0cc0faeef289101a3d7d65c25c7
Legros Morgane <morgane.legros@insa-rouen.fr>
2017-11-07 05:59:44 +0000

    UPDATE littles updates

--- app/classes/Sidebar.jsx
+++ app/classes/Sidebar.jsx
@@ -4,8 +4,8 @@ import axios from 'axios';
 
 import Editor from './Editor.jsx';
 
-function createModule(name) {
-  console.log(name);
+function createModule(module) {
+  console.log(module);
 }
 
 const generateModules = modules => modules.map(module => (
