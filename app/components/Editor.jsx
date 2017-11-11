5e33cbb8c6916a7dd33d6c5713ad89641e209ee2
Thibault Théologien <thibault.theologien@insa-rouen.fr>
2017-11-11 18:08:45 +0100

    UPDATE - default tab controller in editor

--- app/components/Editor.jsx
+++ app/components/Editor.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Menu } from 'semantic-ui-react';
 
 const Editor = props => (
-  <div className="container editor" >
+  <div className="container editor">
+    <Menu attached="top" tabular>
+      <Menu.Item name="Content" />
+      <Menu.Item name="Properties" />
+      <Menu.Item name="Style" />
+    </Menu>
+
     <h1>Edit module {props.module.name}</h1>
     <h2>Content</h2>
     {JSON.stringify(props.module.content)}
