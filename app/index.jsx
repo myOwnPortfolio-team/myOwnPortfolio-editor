ca55ef8512b4ff052dce41de3c16bdc361c15e05
Nistof <nistof.contact@gmail.com>
2017-11-11 12:59:17 +0100

    HOTFIX - Unwanted changes on index.jsx

--- app/index.jsx
+++ app/index.jsx
@@ -1,41 +1,15 @@
 /* global document */
-
+ 
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+ 
 import App from './components/App.jsx';
-
+ 
 import data from './config/app_properties.json';
 import { database, checkModules } from './database';
-
+ 
 import './import.js';
-
-const db = new Dexie('LocalStorage');
-
-// Create schema
-db.version(1).stores({
-  person: 'firstName, lastName',
-});
-
-// Open the database
-db.open();
-
-// Adding data
-db.person.add({
-  firstName: 'f1',
-  lastName: 'l1',
-});
-
-db.person.add({
-  firstName: 'f2',
-  lastName: 'l2',
-});
-
-db.person.add({
-  firstName: 'f3',
-  lastName: 'l3',
-});
-
+ 
 ReactDOM.render(
   <App data={data} database={database} checkModules={checkModules} />,
   document.getElementById('root'),
