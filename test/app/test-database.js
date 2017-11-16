152e065451e71cff2ce8e34d4f9ad97e8564a7db
Nistof <nistof.contact@gmail.com>
2017-11-16 19:04:53 +0100

    UPDATE - Test database definition

--- test/app/test-database.js
+++ test/app/test-database.js
@@ -5,9 +5,28 @@ import Dexie from 'dexie';
 import { database } from '../../app/database';
 
 describe('Database', () => {
-  it('Database exists', () => {
+  let table;
+  try {
+    table = database.table('modules');
+  } catch (error) {
+    table = null;
+  }
+
+  it('Exists', () => {
     Dexie
       .exists('MyOwnPortfolioDB')
       .then(exists => expect(exists).to.equal(true));
+  });
+
+  it('Table "modules" exists', () => {
+    expect(table).not.to.be.a('null');
+  });
+
+  it('Table "modules" minimal schema correct', () => {
+    expect(table.schema.instanceTemplate).to.have.property('name');
+    expect(table.schema.instanceTemplate).to.have.property('sha');
+    expect(table.schema.instanceTemplate).to.have.property('content');
+    expect(table.schema.instanceTemplate).to.have.property('properties');
+    expect(table.schema.instanceTemplate).to.have.property('style');
   });
 });
