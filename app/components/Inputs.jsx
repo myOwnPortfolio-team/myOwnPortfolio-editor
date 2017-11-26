5915192fd9ed1747981a750fb44d117bf1baad87
Legros Morgane <morgane.legros@insa-rouen.fr>
2017-11-26 15:23:34 +0100

    UPDATE inputs form, Form.Field every where

--- app/components/Inputs.jsx
+++ app/components/Inputs.jsx
@@ -1,46 +1,57 @@
 import React from 'react';
-import { Form } from 'semantic-ui-react';
+import { Checkbox, Form, Input, Select, TextArea } from 'semantic-ui-react'
 
 const checkbox = (properties, key, index) => (
   <div>
     <div>{key}</div>
-    <Form.Checkbox key={index} label={properties[key].description} />
+    <Form.Field
+      control={Checkbox}
+      key={index}
+      label={properties[key].description}
+    />
   </div>
 );
 
 const input = (properties, key, index, type) => (
-  <Form.Field key={index}>
-    <label htmlFor={index}>{key}</label>
-    <input id={index} placeholder={properties[key].description} type={type} />
-  </Form.Field>
+  <Form.Field
+    control={Input}
+    key={index}
+    label={key}
+    placeholder={properties[key].description}
+    type={type}
+  />
 );
 
 const textfield = (properties, key, index) => (
-  <Form.Field key={index}>
-    <Form.TextArea id={index} label={key} placeholder={properties[key].description} />
-  </Form.Field>
-);
-
-const select = (properties, key, index, options) => (
-  <Form.Select
+  <Form.Field
+    control={TextArea}
     key={index}
     label={key}
-    options={options}
     placeholder={properties[key].description}
   />
 );
 
+const select = (properties, key, index, options) => (
+  <Form.Field
+    control={Select}
+    key={index}
+    label={key}
+    placeholder={properties[key].description}
+    options={options}
+  />
+);
+
 const slider = (properties, key, index, step) => (
-  <Form.Field key={index}>
-    <label htmlFor={index} title={properties[key].description}>{key}</label>
-    <input
-      id={index}
-      type="range"
-      min={properties[key].minimum}
-      max={properties[key].maximum}
-      step={step}
-    />
-  </Form.Field>
+  <Form.Field
+    control={Input}
+    key={index}
+    label={key}
+    placeholder={properties[key].description}
+    type="range"
+    min={properties[key].minimum}
+    max={properties[key].maximum}
+    step={step}
+  />
 );
 
 
