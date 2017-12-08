bda7d93b085e89de4a019cb71ea42980aa006d27
Legros Morgane <morgane.legros@insa-rouen.fr>
2017-12-08 01:25:37 +0100

    UPDATE - display menu only when there is an active module

--- app/components/Editor.jsx
+++ app/components/Editor.jsx
@@ -20,8 +20,9 @@ class Editor extends React.Component {
   }
 
   render() {
+    const activeSchema = this.props.module.schema[this.state.activeTab];
+
     const editorContent = () => {
-      const activeSchema = this.props.module.schema[this.state.activeTab];
       if (activeSchema === null) {
         return (<div className="editor-empty-content">Create your own porfolio</div>);
       }
@@ -33,26 +34,33 @@ class Editor extends React.Component {
       );
     };
 
+    const menu = () => {
+      if (activeSchema !== null) {
+        return (
+          <Menu attached="top" tabular>
+            <Menu.Item
+              name="Content"
+              active={this.state.activeTab === 'content'}
+              onClick={(e, { name }) => this.handleTabClick(name)}
+            />
+            <Menu.Item
+              name="Properties"
+              active={this.state.activeTab === 'properties'}
+              onClick={(e, { name }) => this.handleTabClick(name)}
+            />
+            <Menu.Item
+              name="Style"
+              active={this.state.activeTab === 'style'}
+              onClick={(e, { name }) => this.handleTabClick(name)}
+            />
+          </Menu>
+        );
+      }
+    };
+
     return (
       <div className="container editor">
-        <Menu attached="top" tabular>
-          <Menu.Item
-            name="Content"
-            active={this.state.activeTab === 'content'}
-            onClick={(e, { name }) => this.handleTabClick(name)}
-          />
-          <Menu.Item
-            name="Properties"
-            active={this.state.activeTab === 'properties'}
-            onClick={(e, { name }) => this.handleTabClick(name)}
-          />
-          <Menu.Item
-            name="Style"
-            active={this.state.activeTab === 'style'}
-            onClick={(e, { name }) => this.handleTabClick(name)}
-          />
-        </Menu>
-
+        {menu()}
         <div className="form editor-content">
           <Form className="form editor-content">
             {editorContent()}
